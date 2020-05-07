f5848bbc9fe4534ddd840ac06c33b27a46e19be5
Wetzel Victor <wetzel.victor@gmail.com>
2020-05-07 10:38:33 +0200

    adding files 6 tubes

---
+++ vocal_tract/CMakeFiles/vocal_tract.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vocal_tract.dir/src/core.cpp.o"
+  "CMakeFiles/vocal_tract.dir/src/parameters.cpp.o"
+  "CMakeFiles/vocal_tract.dir/src/simu.cpp.o"
+  "bin/vocal_tract"
+  "bin/vocal_tract.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vocal_tract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vocal_tract_solve/build/CMakeFiles/vocal_tract.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/src/core.cpp" "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/build/CMakeFiles/vocal_tract.dir/src/core.cpp.o"
+  "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/src/parameters.cpp" "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/build/CMakeFiles/vocal_tract.dir/src/parameters.cpp.o"
+  "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/src/simu.cpp" "/home/victorw/ownCloud/IRCAM/03_DOCS/28_constraints_by_projections/02_python/solving_constraints_vocal_tract/simulations_test/03_data/vocal_tract/build/CMakeFiles/vocal_tract.dir/src/simu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
